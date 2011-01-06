5baa3792b4e8634047f44ce40e1a8a67fa1d9cc1
dcazzulino <none@none>
2011-01-06 19:11:22 -0300

    removed unused stuff.

--- drop-all.ps1
+++ drop-all.ps1
@@ -1,9 +1,6 @@
 # Need XLinq to read the nuspec files to know where to copy them
 [System.Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq") | out-null
 $ns = [System.Xml.Linq.XNamespace]::Get("http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd")
-
-# Locate the nuget command line
-$nuget = Get-ChildItem -Filter NuGet.exe -Recurse
 $msbuild = [System.Environment]::ExpandEnvironmentVariables("%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe")
 
 Write-Progress -Activity "Deploying NETFx" -Status "Cleaning drop location"
@@ -34,16 +31,3 @@ foreach ($build in (Get-ChildItem Extensions -Recurse -Filter Build.csproj))
         $extensionDir.GetDirectories("NuGet")[0].MoveTo($extensionDir.FullName + "\" + $version)
     popd
 }
-
-# Copy all built packages to the Drops folder.
-# $specs = Get-ChildItem Extensions -Recurse -Filter Package.nuspec | Where-Object { $_.DirectoryName.EndsWith("bin\NuGet") }
-
-# foreach ($spec in $specs)
-# {
-#     $doc = [System.Xml.Linq.XDocument]::Load($spec.FullName)
-#     $id = $doc.Root.Element($ns + "metadata").Element($ns + "id").Value
-# 	$version = $doc.Root.Element($ns + "metadata").Element($ns + "version").Value
-# 
-#     $extensionDir = $dropDir.CreateSubdirectory($id)
-#     Copy-Item -Path $spec.Directory -Destination $versionDir -Recurse
-# }
