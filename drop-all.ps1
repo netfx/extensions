38d9503591e7d8efccb5a73175f2402bf28c0d1d
dcazzulino <none@none>
2011-01-18 01:11:08 -0300

    Updated nuget cmdline tool. Added bare logo for packages.

--- drop-all.ps1
+++ drop-all.ps1
@@ -1,7 +1,4 @@
 # Need XLinq to read the nuspec files to know where to copy them
-[System.Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq") | out-null
-$ns = [System.Xml.Linq.XNamespace]::Get("http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd")
-
 # Locate the nuget command line
 $nuget = Get-ChildItem -Filter NuGet.exe -Recurse
 $msbuild = [System.Environment]::ExpandEnvironmentVariables("%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe")
@@ -10,8 +7,6 @@ Write-Progress -Activity "Deploying NETFx" -Status "Cleaning drop location"
 Remove-Item -Path Drop -Force -Recurse -ErrorAction SilentlyContinue | out-null
 Remove-Item Drop -Recurse -ErrorAction SilentlyContinue | out-null
 $dropDir = [System.IO.Directory]::CreateDirectory("Drop") 
-$packagesSource = $dropDir.CreateSubdirectory("PackageSources")
-$packagesRepo = $dropDir.CreateSubdirectory("Repository")
 
 # Build all extensions
 foreach ($build in (Get-ChildItem Extensions -Recurse -Filter Build.csproj))
@@ -24,18 +19,9 @@ foreach ($build in (Get-ChildItem Extensions -Recurse -Filter Build.csproj))
     
         cd bin\NuGet
         $spec = Get-ChildItem "Package.nuspec"
-        $doc = [System.Xml.Linq.XDocument]::Load($spec.FullName)
-        $id = $doc.Root.Element($ns + "metadata").Element($ns + "id").Value
-    	$version = $doc.Root.Element($ns + "metadata").Element($ns + "version").Value
-        $extensionDir = $packagesSource.CreateSubdirectory($id)
 
-        # Write-Progress -ParentId 1 -Activity "Copying package to drop location" -Status ("Copying from " + $spec.Directory + " to " + $versionDir)
-        
-        Copy-Item -Path $spec.Directory -Destination $extensionDir -Recurse
-        $extensionDir.GetDirectories("NuGet")[0].MoveTo($extensionDir.FullName + "\" + $version)
-        
         Write-Progress -Activity "Deploying NETFx" -Status ("Compiling package " + $spec.Name) -PercentComplete $progress
-        &($nuget.FullName) "pack" $spec.FullName -BasePath $spec.Directory.FullName -OutputDirectory $packagesRepo.FullName | out-null
+        &($nuget.FullName) "pack" $spec.FullName -BasePath $spec.Directory.FullName -OutputDirectory $dropDir.FullName | out-null
     popd
 }
 
