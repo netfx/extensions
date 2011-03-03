51dcfb625e45ae83d980d24578f3c361957e6351
dcazzulino <none@none>
2011-03-03 02:49:48 -0300

    Initial version of the static reflection functionality.

---
+++ Extensions/System/Reflection/Reflect/Build/Pack.cmd
@@ -0,0 +1,3 @@
+pushd ..\..\..\..\..\packages\NuGet.CommandLine.1.1.2113.118\Tools
+NuGet.exe pack ..\..\..\Extensions\System\Reflection\Reflect\Build\bin\NuGet\Package.nuspec -BasePath ..\..\..\Extensions\System\Reflection\Reflect\Build\bin\NuGet -OutputDirectory ..\..\..\Extensions\System\Reflection\Reflect\Build\bin
+popd
