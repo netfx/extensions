21e2c3f8e51d154428d2f895a1b77e0389d2a8a9
dcazzulino <none@none>
2011-05-20 10:21:47 -0300

    removing

--- Extensions/System/ComponentModelExtensions/Build/NuGet/tools/install.ps1
+++
@@ -1,4 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-	$project.Object.References.Add("Microsoft.VisualStudio.ComponentModelHost")
-	$project.Object.References.Add("System.ComponentModel.Composition")
--- Extensions/System/ComponentModelExtensions/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\packages\NuGet.CommandLine.1.1.2113.118\Tools
-NuGet.exe pack ..\..\..\Extensions\System\ComponentModelExtensions\Build\bin\NuGet\Package.nuspec -BasePath ..\..\..\Extensions\System\ComponentModelExtensions\Build\bin\NuGet -OutputDirectory ..\..\..\Extensions\System\ComponentModelExtensions\Build\bin
-popd
