0b85276052a82b11bb1777cc061865561c14e268
dcazzulino <none@none>
2011-06-11 19:26:54 -0300

    added script to install renamed package instead

---
+++ Extensions/Microsoft/ApplicationServer/Http/HttpWebService/Build/NuGet/tools/install.ps1
@@ -0,0 +1,4 @@
+﻿param($installPath, $toolsPath, $package, $project)
+	write-warning This package has been replaced with netfx-WebApi.Testing. Installing the right one now.
+	uninstall-package netfx-Microsoft.ApplicationServer.Http.HttpWebService -ProjectName $project.Name
+	install-package netfx-WebApi.Testing -ProjectName $project.Name
