28d22263be3ed6c6e67f764d187b3b6227bf37e9
dcazzulino <none@none>
2011-06-11 18:01:52 -0300

    Renamed HttpWebService to WebApi.Testing, extracted useful classes outside of HttpWebService and documented the APIs better. 88% code coverage too :)

---
+++ Extensions/WebApi/Testing/Build/Pack.cmd
@@ -0,0 +1,3 @@
+pushd ..\..\..\..\
+NuGet.exe pack Extensions\WebApi\Testing\Build\bin\NuGet\Package.nuspec -BasePath Extensions\WebApi\Testing\Build\bin\NuGet -OutputDirectory Extensions\WebApi\Testing\Build\bin
+popd
