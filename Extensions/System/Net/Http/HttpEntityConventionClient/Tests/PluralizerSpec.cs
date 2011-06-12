5bcb07b4459ae0e42dec905ff7429f2af5da5241
dcazzulino <none@none>
2011-06-12 16:03:07 -0300

    Updated namespace and reference to queryable service.

--- Extensions/System/Net/Http/HttpEntityConventionClient/Tests/PluralizerSpec.cs
+++ Extensions/System/Net/Http/HttpEntityConventionClient/Tests/PluralizerSpec.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
-using System.Net.Http.Entity;
 using System.Data.Entity.Design.PluralizationServices;
+using System.Net.Http;
 
 namespace Tests
 {
