adaa76546adcf2bb63cfbd05c9aedc5cccf09878
dcazzulino <none@none>
2011-06-12 14:37:22 -0300

    Renamed formatter

--- Extensions/WebApi/JsonNetMediaTypeFormatter/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\Microsoft\ApplicationServer\Http\JsonNetMediaTypeFormatter\Build\bin\NuGet\Package.nuspec -BasePath Extensions\Microsoft\ApplicationServer\Http\JsonNetMediaTypeFormatter\Build\bin\NuGet -OutputDirectory Extensions\Microsoft\ApplicationServer\Http\JsonNetMediaTypeFormatter\Build\bin
-popd
