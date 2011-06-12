64784b3d2083bde9a8339c587ad590b43da769b4
dcazzulino <none@none>
2011-06-12 14:37:52 -0300

    Added backward compatibility package for the renamed one

---
+++ Extensions/Microsoft/ApplicationServer/Http/JsonNetMediaTypeFormatter/Build/NuGet/tools/install.ps1
@@ -0,0 +1,4 @@
+﻿param($installPath, $toolsPath, $package, $project)
+	write-warning This package has been replaced with netfx-WebApi.JsonNetFormatter. Installing the right one now.
+	uninstall-package netfx-Microsoft.ApplicationServer.Http.JsonNetMediaTypeFormatter -ProjectName $project.Name
+	install-package netfx-WebApi.JsonNetFormatter -ProjectName $project.Name
