de135b1dc6aad9ca6b451b2b017fb2246fdf9de6
dcazzulino <none@none>
2011-06-15 00:31:01 -0300

    Created HttpNameValueCollection nuget.

---
+++ Extensions/System/Net/Http/HttpNameValueCollection/Build/Pack.cmd
@@ -0,0 +1,3 @@
+pushd ..\..\..\..\..\..\
+NuGet.exe pack Extensions\System\Net\Http\HttpNameValueCollection\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Net\Http\HttpNameValueCollection\Build\bin\NuGet -OutputDirectory Extensions\System\Net\Http\HttpNameValueCollection\Build\bin
+popd
---
+++ Extensions/System/Net/Http/HttpNameValueCollection/Tests/HttpNameValueCollectionSpec.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Net.Http;
+
+internal class HttpNameValueCollectionSpec
+{
+	[Fact]
+	public void WhenCreatingWithAnonymousAdd_ThenCanRetrieveQueryString()
+	{
+		var collection = new HttpNameValueCollection
+		{
+			{ "hello", "world" },
+			{ "tag", ".net" },
+			{ "tag", "wpf" },
+			{ "foo", "bar" },
+		};
+
+		var query = collection.ToString();
+
+		Assert.True(query.IndexOf("tag") != query.LastIndexOf("tag"), "Tag should appear twice");
+		Assert.True(query.Contains("hello=world"));
+	}
+}
