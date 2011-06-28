f82d5e952188c7c9b6c178c59d3c543847d944da
sculli_e <none@none>
2011-06-28 11:14:40 -0300

    netfx-System.Collections.Generic.IEnumerable.Traverse Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/Collections/Generic/IEnumerable/Traverse/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\Collections\Generic\IEnumerable\Traverse\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Collections\Generic\IEnumerable\Traverse\Build\bin\NuGet -OutputDirectory Extensions\System\Collections\Generic\IEnumerable\Traverse\Build\bin
-popd
