7c32ce0ae7f4d78672cee54837d1202f8e2827e8
sculli_e <none@none>
2011-06-28 15:16:53 -0300

    netfx-System.TypeInheritance Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/TypeInheritance/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\
-NuGet.exe pack Extensions\System\TypeInheritance\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\TypeInheritance\Build\bin\NuGet -OutputDirectory Extensions\System\TypeInheritance\Build\bin
-popd
