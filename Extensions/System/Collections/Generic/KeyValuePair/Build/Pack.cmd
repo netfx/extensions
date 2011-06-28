4d99bf355c372558cb9f87ae302089096823ee88
sculli_e <none@none>
2011-06-28 11:24:00 -0300

    netfx-System.Collections.Generic.KeyValuePair Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/Collections/Generic/KeyValuePair/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\Collections\Generic\KeyValuePair\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Collections\Generic\KeyValuePair\Build\bin\NuGet -OutputDirectory Extensions\System\Collections\Generic\KeyValuePair\Build\bin
-popd
