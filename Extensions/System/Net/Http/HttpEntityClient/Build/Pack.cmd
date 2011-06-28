ab825a781248769cf5a706b911ebbb976f655817
sculli_e <none@none>
2011-06-28 12:59:30 -0300

    netfx-System.Net.Http.HttpEntityClient Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/Net/Http/HttpEntityClient/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\Net\Http\HttpEntityClient\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Net\Http\HttpEntityClient\Build\bin\NuGet -OutputDirectory Extensions\System\Net\Http\HttpEntityClient\Build\bin
-popd
