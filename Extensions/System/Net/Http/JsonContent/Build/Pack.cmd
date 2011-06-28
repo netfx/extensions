c2aa698f5adfc6003f0ce302936b07883d8d4308
sculli_e <none@none>
2011-06-28 13:53:31 -0300

    netfx-System.Net.Http.JsonContent Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/Net/Http/JsonContent/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\Net\Http\JsonContent\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Net\Http\JsonContent\Build\bin\NuGet -OutputDirectory Extensions\System\Net\Http\JsonContent\Build\bin
-popd
