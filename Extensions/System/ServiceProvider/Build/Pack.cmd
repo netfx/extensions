134ee03dfbc5d8cfd0ee669ce85ec69a095fd1fa
sculli_e <none@none>
2011-06-28 14:35:22 -0300

    netfx-System.ServiceProvider Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/ServiceProvider/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\
-NuGet.exe pack Extensions\System\ServiceProvider\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\ServiceProvider\Build\bin\NuGet -OutputDirectory Extensions\System\ServiceProvider\Build\bin
-popd
--- Extensions/System/ServiceProvider/Source/content/netfx/System/ServiceProvider.cs
+++ Extensions/System/ServiceProvider/Source/content/netfx/System/ServiceProvider.cs
@@ -28,8 +28,9 @@ internal static class ServiceProviderExtensions
 	/// <summary>
 	/// Gets type-based services from the  service provider.
 	/// </summary>
+	/// <nuget id="netfx-System.ServiceProvider" />
 	/// <typeparam name="T">The type of the service to get.</typeparam>
-	/// <param name="provider">The service provider.</param>
+	/// <param name="provider" this="true">The service provider.</param>
 	/// <returns>The requested service, or a <see langword="null"/> reference if the service could not be located.</returns>
 	public static T TryGetService<T>(this IServiceProvider provider)
 	{
@@ -39,8 +40,9 @@ internal static class ServiceProviderExtensions
 	/// <summary>
 	/// Gets type-based services from the  service provider.
 	/// </summary>
+	/// <nuget id="netfx-System.ServiceProvider" />
 	/// <typeparam name="T">The type of the service to get.</typeparam>
-	/// <param name="provider">The service provider.</param>
+	/// <param name="provider" this="true">The service provider.</param>
 	/// <returns>The requested service, or throws an <see cref="InvalidOperationException"/> 
 	/// if the service was not found.</returns>
 	public static T GetService<T>(this IServiceProvider provider)
@@ -59,9 +61,10 @@ internal static class ServiceProviderExtensions
 	/// <summary>
 	/// Gets type-based services from the service provider.
 	/// </summary>
+	/// <nuget id="netfx-System.ServiceProvider" />
 	/// <typeparam name="TRegistration">The type of the registration of the service.</typeparam>
 	/// <typeparam name="TService">The type of the service to get.</typeparam>
-	/// <param name="provider">The service provider.</param>
+	/// <param name="provider" this="true">The service provider.</param>
 	/// <returns>The requested service, or a <see langword="null"/> reference if the service could not be located.</returns>
 	public static TService TryGetService<TRegistration, TService>(this IServiceProvider provider)
 	{
@@ -71,9 +74,10 @@ internal static class ServiceProviderExtensions
 	/// <summary>
 	/// Gets type-based services from the service provider.
 	/// </summary>
+	/// <nuget id="netfx-System.ServiceProvider" />
 	/// <typeparam name="TRegistration">The type of the registration of the service.</typeparam>
 	/// <typeparam name="TService">The type of the service to get.</typeparam>
-	/// <param name="provider">The service provider.</param>
+	/// <param name="provider" this="true">The service provider.</param>
 	/// <returns>The requested service, or throws an <see cref="InvalidOperationException"/> 
 	/// if the service was not found.</returns>
 	public static TService GetService<TRegistration, TService>(this IServiceProvider provider)
