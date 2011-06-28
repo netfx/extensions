ab1bfa94e295e0ac2473562ae7aa761bfe9b8b28
sculli_e <none@none>
2011-06-28 15:24:02 -0300

    netfx-System.Windows.Input.DelegateCommand Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/Windows/Input/DelegateCommand/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\Windows\Input\DelegateCommand\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Windows\Input\DelegateCommand\Build\bin\NuGet -OutputDirectory Extensions\System\Windows\Input\DelegateCommand\Build\bin
-popd
