6071e5b4700f209f5b2dc3ea4f0618baa347b3b8
sculli_e <none@none>
2011-06-28 13:44:20 -0300

    netfx-System.Net.Http.HttpNameValueCollection Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/Net/Http/HttpNameValueCollection/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\Net\Http\HttpNameValueCollection\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\Net\Http\HttpNameValueCollection\Build\bin\NuGet -OutputDirectory Extensions\System\Net\Http\HttpNameValueCollection\Build\bin
-popd
