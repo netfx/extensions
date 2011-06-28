976eac65b1fc45e3dabfe38476ee8a253a274e5c
sculli_e <none@none>
2011-06-28 14:52:51 -0300

    netfx-System.StringFormatWith Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/StringFormatWith/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\
-NuGet.exe pack Extensions\System\StringFormatWith\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\StringFormatWith\Build\bin\NuGet -OutputDirectory Extensions\System\StringFormatWith\Build\bin
-popd
