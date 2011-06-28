82d9ba58d7930fd5ed1dfb2ad181bd30187bde65
sculli_e <none@none>
2011-06-28 12:09:09 -0300

    netfx-System.IO.Packaging.StreamUnpack Added documentation and xml attributes to be discoverable by NuSense. Fixed project to the new structure.

--- Extensions/System/IO/Packaging/StreamUnpack/Build/Pack.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\System\IO\Packaging\StreamUnpack\Build\bin\NuGet\Package.nuspec -BasePath Extensions\System\IO\Packaging\StreamUnpack\Build\bin\NuGet -OutputDirectory Extensions\System\IO\Packaging\StreamUnpack\Build\bin
-popd
