1316050c124019b9f5cfe421ed6141048995956a
Daniel@KZU <Daniel@KZU>
2011-07-08 02:03:14 -0300

    updated appserver for backs compat nugets.

--- Extensions/Microsoft/ApplicationServer/Http/HttpWebService/Build/Pack.cmd
+++ Extensions/Microsoft/ApplicationServer/Http/HttpWebService/Build/Pack.cmd
@@ -1,3 +1,1 @@
-pushd ..\..\..\..\..\..\
-NuGet.exe pack Extensions\Microsoft\ApplicationServer\Http\HttpWebService\Build\bin\NuGet\Package.nuspec -BasePath Extensions\Microsoft\ApplicationServer\Http\HttpWebService\Build\bin\NuGet -OutputDirectory Extensions\Microsoft\ApplicationServer\Http\HttpWebService\Build\bin
-popd
+ErrorGeneratingOutput
--- Extensions/Microsoft/IdentityModel/Swt/Build/Pack.cmd
+++ Extensions/Microsoft/IdentityModel/Swt/Build/Pack.cmd
@@ -1,3 +1,1 @@
-pushd ..\..\..\..\..\
-NuGet.exe pack Extensions\Microsoft\IdentityModel\Swt\Build\bin\NuGet\Package.nuspec -BasePath Extensions\Microsoft\IdentityModel\Swt\Build\bin\NuGet -OutputDirectory Extensions\Microsoft\IdentityModel\Swt\Build\bin
-popd
+ErrorGeneratingOutput
