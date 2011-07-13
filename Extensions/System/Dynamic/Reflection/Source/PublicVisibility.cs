f8aad64242d5020de2e889dc44fa31b60045149b
Daniel@KZU <Daniel@KZU>
2011-07-13 09:20:36 -0300

    Added public visibility and API doc for dynamic reflection.

---
+++ Extensions/System/Dynamic/Reflection/Source/PublicVisibility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Dynamic
+{
+	// Makes types visible so that the documentation XML is generated.
+	public partial class DynamicReflection { }
+	public partial class OutValue { }
+	public partial class OutValue<T> { }
+	public partial class RefValue { }
+	public partial class RefValue<T> { }
+	public partial class TypeParameter { }
+}
