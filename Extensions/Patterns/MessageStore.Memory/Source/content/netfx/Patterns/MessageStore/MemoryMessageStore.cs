0e551cfa09a52c1e3bd92a3482bb455de5c64419
kzu <none@none>
2011-09-02 10:52:45 -0300

    Added in-memory message store

---
+++ Extensions/Patterns/MessageStore.Memory/Source/content/netfx/Patterns/MessageStore/MemoryMessageStore.cs
@@ -0,0 +1,163 @@
+﻿#region BSD License
+/* 
+Copyright (c) 2010, NETFx
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+* Neither the name of Clarius Consulting nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+	/// <summary>
+	/// Simple in-memory message store.
+	/// </summary>
+partial class MemoryMessageStore<TBaseMessage> : IMessageStore<TBaseMessage>
+{
+	private List<StoredMessage> messages = new List<StoredMessage>();
+	private Func<DateTime> utcNow;
+
+	public MemoryMessageStore()
+		: this(() => DateTime.UtcNow)
+	{
+	}
+
+	public MemoryMessageStore(Func<DateTime> utcNow)
+	{
+		this.TypeNameConverter = type => type.Name;
+		this.utcNow = utcNow;
+	}
+
+	public Func<Type, string> TypeNameConverter { get; set; }
+	public IQueryable<StoredMessage> AllMessages { get { return this.messages.AsQueryable(); } }
+
+	public void Persist(TBaseMessage message, IDictionary<string, object> headers)
+	{
+		this.messages.Add(new StoredMessage(message, headers) { Timestamp = this.utcNow() });
+	}
+
+	public void Commit()
+	{
+	}
+
+	public IEnumerable<TBaseMessage> Query(MessageStoreQueryCriteria criteria)
+	{
+		var source = this.messages.AsQueryable();
+		var predicate = ToExpression(criteria, this.TypeNameConverter);
+
+		if (predicate != null)
+			source = source.Where(predicate).Cast<StoredMessage>();
+
+		return source.Select(x => x.Message);
+	}
+
+	public class StoredMessage
+	{
+		public StoredMessage(TBaseMessage message, IDictionary<string, object> headers)
+		{
+			this.Message = message;
+			this.Headers = headers;
+			this.MessageId = Guid.NewGuid();
+		}
+
+		public IDictionary<string, object> Headers { get; set; }
+		public TBaseMessage Message { get; private set; }
+		public Guid MessageId { get; private set; }
+		public string MessageType { get { return this.Message.GetType().Name; } }
+		public DateTime Timestamp { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("{0}({1}), at {2} (payload: {3})",
+				this.MessageType,
+				this.MessageId,
+				this.Timestamp,
+				this.Message);
+		}
+	}
+
+	private static Expression<Func<StoredMessage, bool>> ToExpression(MessageStoreQueryCriteria criteria, Func<Type, string> typeNameConverter)
+	{
+		return new StoredEventCriteriaBuilder(criteria, typeNameConverter).Build();
+	}
+
+	private class StoredEventCriteriaBuilder
+	{
+		private MessageStoreQueryCriteria criteria;
+		private Func<Type, string> typeNameConverter;
+
+		public StoredEventCriteriaBuilder(MessageStoreQueryCriteria criteria, Func<Type, string> typeNameConverter)
+		{
+			this.criteria = criteria;
+			this.typeNameConverter = typeNameConverter;
+		}
+
+		private Expression<Func<StoredMessage, bool>> AddEventFilter(Expression<Func<StoredMessage, bool>> result)
+		{
+			var criteria = default(Expression<Func<StoredMessage, bool>>);
+
+			foreach (var filter in this.criteria.MessageTypes)
+			{
+				var sourceType = typeNameConverter.Invoke(filter);
+
+				// ORs all aggregregate filters.
+				criteria = Or(criteria, e => e.MessageType == sourceType);
+			}
+
+			if (criteria == null)
+				return result;
+
+			// AND the criteria built so far.
+			return And(result, criteria);
+		}
+
+		private Expression<Func<StoredMessage, bool>> And(Expression<Func<StoredMessage, bool>> left, Expression<Func<StoredMessage, bool>> right)
+		{
+			return left == null ? right : left.And(right);
+		}
+
+		private Expression<Func<StoredMessage, bool>> Or(Expression<Func<StoredMessage, bool>> left, Expression<Func<StoredMessage, bool>> right)
+		{
+			return left == null ? right : left.Or(right);
+		}
+
+		/// <summary>
+		/// Builds the expression for the criteria.
+		/// </summary>
+		public Expression<Func<StoredMessage, bool>> Build()
+		{
+			var result = default(Expression<Func<StoredMessage, bool>>);
+			result = AddEventFilter(result);
+
+			if (this.criteria.Since != null)
+			{
+				var since = this.criteria.Since.Value.ToUniversalTime();
+				if (this.criteria.IsExclusiveRange)
+					result = And(result, e => e.Timestamp > since);
+				else
+					result = And(result, e => e.Timestamp >= since);
+			}
+
+			if (this.criteria.Until != null)
+			{
+				var until = this.criteria.Until.Value.ToUniversalTime();
+				if (this.criteria.IsExclusiveRange)
+					result = And(result, e => e.Timestamp < until);
+				else
+					result = And(result, e => e.Timestamp <= until);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Extensions/Patterns/MessageStore.Memory/Tests/content/netfx/Patterns/MessageStore/MessageStoreQuerySpec.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Collections;
+using Moq;
+using System.Threading;
+
+namespace NetFx.Patterns.MessageStore.Memory
+{
+	public class MemoryMessageStoreSpec
+	{
+		private MemoryMessageStore<Message> store;
+		private Func<DateTime> utcNow = () => DateTime.UtcNow;
+		private IDictionary<string, object> emptyHeaders = new Dictionary<string, object>();
+
+		public MemoryMessageStoreSpec()
+		{
+			this.store = new MemoryMessageStore<Message>(() => this.utcNow());
+
+			this.store.Persist(new CreateProduct { Id = 5, Title = "DevStore" }, emptyHeaders);
+			this.store.Persist(new PublishProduct { Version = 1 }, emptyHeaders);
+			this.store.Persist(new PublishProduct { Version = 2 }, emptyHeaders);
+			this.store.Persist(new PublishProduct { Version = 3 }, emptyHeaders);
+
+			this.store.Persist(new CreateProduct { Id = 6, Title = "WoVS" }, emptyHeaders);
+			this.store.Persist(new PublishProduct { Version = 1 }, emptyHeaders);
+			this.store.Persist(new PublishProduct { Version = 2 }, emptyHeaders);
+		}
+
+		[Fact]
+		public void WhenCommitting_ThenNoOp()
+		{
+			this.store.Commit();
+		}
+
+		[Fact]
+		public void WhenNoDateTimeFuncProvided_ThenUsesUtcNow()
+		{
+			this.store = new MemoryMessageStore<Message>();
+
+			this.store.Persist(new CreateProduct { Id = 5, Title = "DevStore" }, emptyHeaders);
+
+			Thread.Sleep(1);
+
+			Assert.True(DateTime.UtcNow.Ticks > this.store.AllMessages.First().Timestamp.Ticks);
+		}
+
+		[Fact]
+		public void WhenGettingEmptyQueryEnumerable_ThenEmptyList()
+		{
+			var store = new MemoryMessageStore<Message>(() => this.utcNow());
+			var enumerable = store.Query() as IEnumerable;
+
+			Assert.False(enumerable.GetEnumerator().MoveNext());
+		}
+
+		[Fact]
+		public void WhenGettingAllMessages_ThenSucceeds()
+		{
+			var messages = store.AllMessages.Select(x => x.ToString());
+
+			Assert.Equal(7, messages.Count());
+		}
+
+		[Fact]
+		public void WhenFilteringByMessageType_ThenSucceeds()
+		{
+			var messages = store.Query().OfType<CreateProduct>();
+
+			Assert.Equal(2, messages.Count());
+		}
+
+		[Fact]
+		public void WhenFilteringByMessageType_ThenCanAccessCriteria()
+		{
+			var messages = store.Query().OfType<CreateProduct>();
+
+			Assert.Equal(1, messages.Criteria.MessageTypes.Count);
+		}
+
+		[Fact]
+		public void WhenFilteringByDateSince_ThenSucceeds()
+		{
+			var when = DateTime.Today.Subtract(TimeSpan.FromDays(5)).ToUniversalTime();
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(7)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(6)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => when;
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(4)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			var messages = store.Query().OfType<Deactivate>().Since(when);
+
+			Assert.Equal(2, messages.Count());
+		}
+
+		[Fact]
+		public void WhenFilteringByDateSinceExclusive_ThenSucceeds()
+		{
+			var when = DateTime.Today.Subtract(TimeSpan.FromDays(5)).ToUniversalTime();
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(7)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(6)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => when;
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(4)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			var messages = store.Query().OfType<Deactivate>().Since(when).ExclusiveRange();
+
+			Assert.Equal(1, messages.Count());
+		}
+
+		[Fact]
+		public void WhenFilteringByDateUntil_ThenSucceeds()
+		{
+			var when = DateTime.Today.Subtract(TimeSpan.FromDays(5)).ToUniversalTime();
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(7)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(6)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => when;
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(4)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			var messages = store.Query().OfType<Deactivate>().Until(when);
+
+			Assert.Equal(3, messages.Count());
+		}
+
+		[Fact]
+		public void WhenFilteringByDateUntilExclusive_ThenSucceeds()
+		{
+			var when = DateTime.Today.Subtract(TimeSpan.FromDays(5)).ToUniversalTime();
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(7)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(6)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => when;
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			this.utcNow = () => DateTime.Today.Subtract(TimeSpan.FromDays(4)).ToUniversalTime();
+			store.Persist(new Deactivate(), emptyHeaders);
+
+			var messages = store.Query().OfType<Deactivate>().Until(when).ExclusiveRange();
+
+			Assert.Equal(2, messages.Count());
+		}
+
+		public class Deactivate : Message
+		{
+			public override string ToString()
+			{
+				return this.GetType().Name;
+			}
+		}
+
+		public abstract class Message { }
+
+		public class CreateProduct : Message
+		{
+			public int Id { get; set; }
+			public string Title { get; set; }
+
+			public override string ToString()
+			{
+				return string.Format("Created new product with Id={0} and Title='{1}'.",
+					this.Id, this.Title);
+			}
+		}
+
+		public class PublishProduct : Message
+		{
+			public int Version { get; set; }
+
+			public override string ToString()
+			{
+				return "Published new product version " + this.Version + ".";
+			}
+		}
+	}
+}
