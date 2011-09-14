66cc29ed665f444469b893591e1118b522173c39
kzu <none@none>
2011-09-14 10:49:46 -0700

    Created date time to natural time extension methods

---
+++ Extensions/System/Collections/Generic/DateTimeToNaturalTime/Source/content/netfx/System/Collections/Generic/DateTimeToNaturalTime.cs
@@ -0,0 +1,143 @@
+﻿#region BSD License
+/* 
+Copyright (c) 2011, NETFx
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list 
+  of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this 
+  list of conditions and the following disclaimer in the documentation and/or other 
+  materials provided with the distribution.
+
+* Neither the name of Clarius Consulting nor the names of its contributors may be 
+  used to endorse or promote products derived from this software without specific 
+  prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Converts a date time to a readable natural time (i.e. '10 hours', '2 years') or relative time (i.e. '5 days ago').
+/// </summary>
+/// <nuget id="netfx-System.Collections.Generic.DateTimeToNaturalTime"/>
+internal static partial class DateTimeToNaturalTimeExtension
+{
+	/// <summary>
+	/// Renders the given date as a natural language string, appending the
+	/// "ago" suffix like "10 minutes ago".
+	/// </summary>
+	/// <param name="date" this="true">The date to convert.</param>
+	public static string ToNaturalRelativeTime(this DateTime date)
+	{
+		return ToNaturalString(date, true);
+	}
+
+	/// <summary>
+	/// Renders the given date as a natural language string, like "10 minutes".
+	/// </summary>
+	/// <param name="date" this="true">The date to convert.</param>
+	public static string ToNaturalTime(this DateTime date)
+	{
+		return ToNaturalString(date, false);
+	}
+
+	/// <summary>
+	/// Renders the given date as a natural language string, appending the
+	/// "ago" suffix like "10 minutes ago".
+	/// </summary>
+	/// <param name="date" this="true">The date to convert.</param>
+	public static string ToNaturalRelativeTime(this DateTimeOffset date)
+	{
+		return ToNaturalString(date.UtcDateTime, true);
+	}
+
+	/// <summary>
+	/// Renders the given date as a natural language string, like "10 minutes".
+	/// </summary>
+	/// <param name="date" this="true">The date to convert.</param>
+	public static string ToNaturalTime(this DateTimeOffset date)
+	{
+		return ToNaturalString(date.UtcDateTime, false);
+	}
+
+	private static string ToNaturalString(this DateTime date, bool prependAgo)
+	{
+		if (date.Kind == DateTimeKind.Unspecified)
+			throw new NotSupportedException("Date must be in UTC or local time. Unsupported 'Unspecified' date kind.");
+
+		if (date.Kind != DateTimeKind.Utc)
+			date = date.ToUniversalTime();
+
+		var totalSeconds = ((TimeSpan)(DateTime.UtcNow - date)).TotalSeconds;
+
+		var years = Math.Floor(totalSeconds / (365.242199 * 24 * 60 * 60));
+		if (years >= 1)
+		{
+			return string.Format((years == 1 ?
+				(prependAgo ? "{0} year ago" : "{0} year") :
+				(prependAgo ? "{0} years ago" : "{0} years")),
+				years);
+		}
+		var months = Math.Floor(totalSeconds / (30.4368499 * 24 * 60 * 60));
+		if (months >= 1)
+		{
+			return string.Format((months == 1 ?
+				(prependAgo ? "{0} month ago" : "{0} month") :
+				(prependAgo ? "{0} months ago" : "{0} months")),
+				months);
+		}
+
+		var weeks = Math.Floor(totalSeconds / (7 * 24 * 60 * 60));
+		if (weeks >= 1)
+		{
+			return string.Format((weeks == 1 ?
+				(prependAgo ? "{0} week ago" : "{0} week") :
+				(prependAgo ? "{0} weeks ago" : "{0} weeks")),
+				weeks);
+		}
+
+		var days = Math.Floor(totalSeconds / (24 * 60 * 60));
+		if (days >= 1)
+		{
+			return string.Format((days == 1 ?
+				(prependAgo ? "{0} day ago" : "{0} day") :
+				(prependAgo ? "{0} days ago" : "{0} days")),
+				days);
+		}
+
+		var hours = Math.Floor(totalSeconds / (60 * 60));
+		if (hours >= 1)
+		{
+			return string.Format((hours == 1 ?
+				(prependAgo ? "{0} hour ago" : "{0} hour") :
+				(prependAgo ? "{0} hours ago" : "{0} hours")),
+				hours);
+		}
+
+		var minutes = Math.Floor(totalSeconds / 60);
+		if (minutes < 0) minutes = 0;
+
+		return string.Format((minutes == 1 ?
+			(prependAgo ? "{0} min ago" : "{0} min") :
+			(prependAgo ? "{0} mins ago" : "{0} mins")),
+			minutes);
+	}
+}
---
+++ Extensions/System/Collections/Generic/DateTimeToNaturalTime/Tests/content/netfx/System/Collections/Generic/DateTimeToNaturalTimeSpec.cs
@@ -0,0 +1,203 @@
+﻿#region BSD License
+/* 
+Copyright (c) 2011, NETFx
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list 
+  of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this 
+  list of conditions and the following disclaimer in the documentation and/or other 
+  materials provided with the distribution.
+
+* Neither the name of Clarius Consulting nor the names of its contributors may be 
+  used to endorse or promote products derived from this software without specific 
+  prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+public class DateTimeToNaturalTimeSpec
+{
+	[Fact]
+	public void WhenOneYearTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(366));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(366));
+
+		Assert.Equal("1 year", date.ToNaturalTime());
+		Assert.Equal("1 year ago", date.ToNaturalRelativeTime());
+		Assert.Equal("1 year", offset.ToNaturalTime());
+		Assert.Equal("1 year ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenTwoYearsTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(800));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(800));
+
+		Assert.Equal("2 years", date.ToNaturalTime());
+		Assert.Equal("2 years ago", date.ToNaturalRelativeTime());
+		Assert.Equal("2 years", offset.ToNaturalTime());
+		Assert.Equal("2 years ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenOneMonthTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(32));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(32));
+
+		Assert.Equal("1 month", date.ToNaturalTime());
+		Assert.Equal("1 month ago", date.ToNaturalRelativeTime());
+		Assert.Equal("1 month", offset.ToNaturalTime());
+		Assert.Equal("1 month ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenTwoMonthsTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(65));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(65));
+
+		Assert.Equal("2 months", date.ToNaturalTime());
+		Assert.Equal("2 months ago", date.ToNaturalRelativeTime());
+		Assert.Equal("2 months", offset.ToNaturalTime());
+		Assert.Equal("2 months ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenOneWeekTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(8));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(8));
+
+		Assert.Equal("1 week", date.ToNaturalTime());
+		Assert.Equal("1 week ago", date.ToNaturalRelativeTime());
+		Assert.Equal("1 week", offset.ToNaturalTime());
+		Assert.Equal("1 week ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenTwoWeeksTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(15));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(15));
+
+		Assert.Equal("2 weeks", date.ToNaturalTime());
+		Assert.Equal("2 weeks ago", date.ToNaturalRelativeTime());
+		Assert.Equal("2 weeks", offset.ToNaturalTime());
+		Assert.Equal("2 weeks ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenOneDayTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(1));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(1));
+
+		Assert.Equal("1 day", date.ToNaturalTime());
+		Assert.Equal("1 day ago", date.ToNaturalRelativeTime());
+		Assert.Equal("1 day", offset.ToNaturalTime());
+		Assert.Equal("1 day ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenTwoDaysTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromDays(2));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(2));
+
+		Assert.Equal("2 days", date.ToNaturalTime());
+		Assert.Equal("2 days ago", date.ToNaturalRelativeTime());
+		Assert.Equal("2 days", offset.ToNaturalTime());
+		Assert.Equal("2 days ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenOneHourTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromHours(1));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromHours(1));
+
+		Assert.Equal("1 hour", date.ToNaturalTime());
+		Assert.Equal("1 hour ago", date.ToNaturalRelativeTime());
+		Assert.Equal("1 hour", offset.ToNaturalTime());
+		Assert.Equal("1 hour ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenTwoHoursTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromHours(2));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromHours(2));
+
+		Assert.Equal("2 hours", date.ToNaturalTime());
+		Assert.Equal("2 hours ago", date.ToNaturalRelativeTime());
+		Assert.Equal("2 hours", offset.ToNaturalTime());
+		Assert.Equal("2 hours ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenNoMinutesTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromSeconds(10));
+		var offset = DateTime.Now.Subtract(TimeSpan.FromSeconds(10));
+
+		Assert.Equal("0 mins", date.ToNaturalTime());
+		Assert.Equal("0 mins ago", date.ToNaturalRelativeTime());
+		Assert.Equal("0 mins", offset.ToNaturalTime());
+		Assert.Equal("0 mins ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenOneMinuteTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromMinutes(1));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromMinutes(1));
+
+		Assert.Equal("1 min", date.ToNaturalTime());
+		Assert.Equal("1 min ago", date.ToNaturalRelativeTime());
+		Assert.Equal("1 min", offset.ToNaturalTime());
+		Assert.Equal("1 min ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenTwoMinutesTime_ThenRendersNaturalTime()
+	{
+		var date = DateTime.Now.Subtract(TimeSpan.FromMinutes(2));
+		var offset = DateTimeOffset.Now.Subtract(TimeSpan.FromMinutes(2));
+
+		Assert.Equal("2 mins", date.ToNaturalTime());
+		Assert.Equal("2 mins ago", date.ToNaturalRelativeTime());
+		Assert.Equal("2 mins", offset.ToNaturalTime());
+		Assert.Equal("2 mins ago", offset.ToNaturalRelativeTime());
+	}
+
+	[Fact]
+	public void WhenDateKindUnspecified_ThenThrowsNotSupported()
+	{
+		var date = new DateTime(2011, 1, 1, 1, 1, 1, DateTimeKind.Unspecified);
+
+		Assert.Throws<NotSupportedException>(() => date.ToNaturalTime());
+	}
+}
