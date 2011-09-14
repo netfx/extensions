e0e4bf703049a1d8796e42049f9454319a10e484
kzu <none@none>
2011-09-14 10:45:13 -0700

    Added XmlEncode and XmlDecode packages

---
+++ Extensions/System/Xml/XmlDecode/Source/content/netfx/System/Xml/XmlDecode.cs
@@ -0,0 +1,63 @@
+﻿#region BSD License
+/* 
+Copyright (c) 2011, NETFx
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list 
+  of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this 
+  list of conditions and the following disclaimer in the documentation and/or other 
+  materials provided with the distribution.
+
+* Neither the name of Clarius Consulting nor the names of its contributors may be 
+  used to endorse or promote products derived from this software without specific 
+  prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+
+///	<nuget id="netfx-System.Xml.XmlDecode" />
+internal static partial class XmlDecodeExtension
+{
+	/// <summary>
+	/// Decodes an XML encoded string to its normal representation with 
+	/// the entities resolved (i.e. angle brackets in text).
+	/// </summary>
+	/// <param name="value" this="true">The value to decode in XML compatible way.</param>
+	/// <returns>The decoded string.</returns>
+	public static string XmlDecode(this string value)
+	{
+		Guard.NotNull(() => value, value);
+
+		if (value.Length == 0)
+			return value;
+
+		var output = new StringBuilder();
+		var reader = XmlReader.Create(new StringReader(value), new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment });
+		reader.MoveToContent();
+		var text = (XText)XText.ReadFrom(reader);
+
+		return text.Value;
+	}
+}
