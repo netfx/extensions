da0013239aaa8dc78fa869682b93d1417da76123
kzu <none@none>
2012-01-11 11:00:24 -0300

    Added nuget restore functionality. Added one more unit test.

--- Extensions/System/Threading/ThreadData/Tests/content/netfx/System/Threading/ThreadDataSpec.cs
+++ Extensions/System/Threading/ThreadData/Tests/content/netfx/System/Threading/ThreadDataSpec.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using System.Threading;
+using System.Threading.Tasks;
 
 internal class ThreadDataSpec
 {
@@ -59,6 +60,28 @@ internal class ThreadDataSpec
 		var saved = ThreadData.GetData<Foo>();
 
 		Assert.Null(saved);
+	}
+
+	[Fact]
+	public void WhenMultipleThreadsSetData_ThenEachAccessesDifferentData()
+	{
+		Action action = () =>
+		{
+			var data = ThreadData.GetData<Foo>();
+			Assert.Null(data);
+
+			data = new Foo { Id = Thread.CurrentThread.ManagedThreadId };
+			ThreadData.SetData(data);
+
+			var threadData = ThreadData.GetData<Foo>();
+
+			Assert.NotNull(threadData);
+			Assert.Same(threadData, data);
+		};
+
+		var tasks = Enumerable.Range(0, 20).Select(x => Task.Factory.StartNew(action)).ToArray();
+
+		Task.WaitAll(tasks);
 	}
 
 	public class Foo
