95d67457c79f5475ccacc4c8c931155e5928784c
kzu <none@none>
2012-06-19 17:01:52 -0300

    Added initial version of the ForEach extension method

---
+++ Extensions/System/Collections/Generic/IEnumerable/ForEach/Source/content/netfx/System/Collections/Generic/IEnumerable/ForEach.cs
@@ -0,0 +1,54 @@
+﻿#region BSD License
+/* 
+Copyright (c) 2011, NETFx
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list 
+  of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this 
+  list of conditions and the following disclaimer in the documentation and/or other 
+  materials provided with the distribution.
+
+* Neither the name of Clarius Consulting nor the names of its contributors may be 
+  used to endorse or promote products derived from this software without specific 
+  prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE.
+*/
+#endregion
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Exposes a ForEach&lt;T&gt; extension method on IEnumerable&lt;T&gt;, just like List&lt;T&gt; does.
+    /// </summary>
+    ///	<nuget id="netfx-System.Collections.Generic.IEnumerable.ForEach" />
+    static partial class ForEachExtension
+    {
+        /// <summary>
+        /// Iterates all source elements and executes the given action for each.
+        /// </summary>
+        /// <param name="source" this="true">The source of items to iterate.</param>
+        /// <param name="action">The action to execute for each item in the source.</param>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
