79f768171b63c9994d583a3f10a48c5efe0981ce
kzu <none@none>
2012-08-08 10:53:06 -0300

    Removed hardcoded class visibility.

--- Extensions/System/Guard/Source/content/netfx/System/Guard.cs
+++ Extensions/System/Guard/Source/content/netfx/System/Guard.cs
@@ -38,35 +38,35 @@ using System.Linq.Expressions;
 /// </summary>
 ///	<nuget id="netfx-Guard" />
 [DebuggerStepThrough]
-internal static class Guard
+static class Guard
 {
-	/// <summary>
-	/// Ensures the given <paramref name="value"/> is not null.
-	/// Throws <see cref="ArgumentNullException"/> otherwise.
-	/// </summary>
-	public static void NotNull<T>(Expression<Func<T>> reference, T value)
-	{
-		if (value == null)
-			throw new ArgumentNullException(GetParameterName(reference), "Parameter cannot be null.");
-	}
+    /// <summary>
+    /// Ensures the given <paramref name="value"/> is not null.
+    /// Throws <see cref="ArgumentNullException"/> otherwise.
+    /// </summary>
+    public static void NotNull<T>(Expression<Func<T>> reference, T value)
+    {
+        if (value == null)
+            throw new ArgumentNullException(GetParameterName(reference), "Parameter cannot be null.");
+    }
 
-	/// <summary>
-	/// Ensures the given string <paramref name="value"/> is not null or empty.
-	/// Throws <see cref="ArgumentNullException"/> in the first case, or 
-	/// <see cref="ArgumentException"/> in the latter.
-	/// </summary>
-	public static void NotNullOrEmpty(Expression<Func<string>> reference, string value)
-	{
-		NotNull<string>(reference, value);
-		if (value.Length == 0)
-			throw new ArgumentException(GetParameterName(reference), "Parameter cannot be empty.");
-	}
+    /// <summary>
+    /// Ensures the given string <paramref name="value"/> is not null or empty.
+    /// Throws <see cref="ArgumentNullException"/> in the first case, or 
+    /// <see cref="ArgumentException"/> in the latter.
+    /// </summary>
+    public static void NotNullOrEmpty(Expression<Func<string>> reference, string value)
+    {
+        NotNull<string>(reference, value);
+        if (value.Length == 0)
+            throw new ArgumentException(GetParameterName(reference), "Parameter cannot be empty.");
+    }
 
-	private static string GetParameterName(Expression reference)
-	{
-		var lambda = reference as LambdaExpression;
-		var member = lambda.Body as MemberExpression;
+    private static string GetParameterName(Expression reference)
+    {
+        var lambda = reference as LambdaExpression;
+        var member = lambda.Body as MemberExpression;
 
-		return member.Member.Name;
-	}
+        return member.Member.Name;
+    }
 }
