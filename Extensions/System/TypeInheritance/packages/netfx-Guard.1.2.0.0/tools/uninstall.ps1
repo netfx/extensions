7fab59c5763eb1fc50d4715eb534ce4d1b9f00f2
kzu <none@none>
2012-08-08 12:26:03 -0300

    Removed all packages since we're moving to nuget package restore.

--- Extensions/System/TypeInheritance/packages/netfx-Guard.1.2.0.0/tools/uninstall.ps1
+++
@@ -1,4 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-	remove-item ([System.Environment]::ExpandEnvironmentVariables("%VisualStudioDir%\Code Snippets\Visual C#\My Code Snippets\notnull.snippet"))
-	remove-item ([System.Environment]::ExpandEnvironmentVariables("%VisualStudioDir%\Code Snippets\Visual C#\My Code Snippets\notempty.snippet"))	
