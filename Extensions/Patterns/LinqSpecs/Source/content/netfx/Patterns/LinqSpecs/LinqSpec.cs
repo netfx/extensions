5388340ea2c33c5c2cb732189afb195f5f8ad8cb
Felix Collins <felix@intranel.com>
2015-12-07 15:41:11 +1300

    Introduced OnProperty helper to allow mixing specs of differing types where the second type is a property on the first.

--- Extensions/Patterns/LinqSpecs/Source/content/netfx/Patterns/LinqSpecs/LinqSpec.cs
+++ Extensions/Patterns/LinqSpecs/Source/content/netfx/Patterns/LinqSpecs/LinqSpec.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Linq.Expressions;
 using Linq = System.Linq.Expressions;
-using System.Collections.Generic;
 
 /// <summary>
 /// Allows creating and combining query specifications using logical And and Or 
@@ -13,22 +11,35 @@ static partial class LinqSpec
 	/// <summary>
 	/// Creates a custom ad-hoc <see cref="LinqSpec{T}"/> for the given <typeparamref name="T"/>.
 	/// </summary>
-	public static LinqSpec<T> For<T>(Expression<Func<T, bool>> specification)
+	public static LinqSpec<T> For<T>(Linq.Expression<Func<T, bool>> specification)
 	{
 		return specification;
 	}
 
 	/// <summary>
-	/// Converts the given expression to a linq query specification. Typically
-	/// not needed as the expression can be converted implicitly to a linq
+	/// Converts the given expression to a linq query specification. Typically 
+	/// not needed as the expression can be converted implicitly to a linq 
 	/// specification by just assigning it or passing it as such to another method.
 	/// </summary>
-	/// <typeparam name="T">Type of specification value being constrained.</typeparam>
-	/// <param name="specification" this="true">The specification expression to convert to a spec.</param>
-	/// <nuget id="netfx-Patterns.LinqSpecs"/>
-	public static LinqSpec<T> Spec<T>(this Expression<Func<T, bool>> specification)
+	public static LinqSpec<T> Spec<T>(this Linq.Expression<Func<T, bool>> specification)
 	{
 		return specification;
+	}
+
+	/// <summary>
+	/// Allows reusing a specification for another Type by applying it to a property
+	/// of the Type being targetted by the new specification.
+	/// </summary>
+	/// <typeparam name="T">The type of the new LinqSpec to be created</typeparam>
+	/// <typeparam name="TNestedEntity">The type of the property on the T which is being accessed</typeparam>
+	/// <param name="propertyExpression">The property expression to access property on T. Usually a lambda like t => t.MyProp</param>
+	/// <param name="spec">A LinqSpec for type TNestedEntity</param>
+	public static LinqSpec<T> OnProperty<T, TNestedEntity>(Linq.Expression<Func<T, TNestedEntity>> propertyExpression, LinqSpec<TNestedEntity> spec)
+	{
+		var replacer = new ParameterReplaceVisitor(spec.Expression.Parameters.First(), propertyExpression.Body);
+		var newExpression = replacer.Visit(spec.Expression.Body);
+		var exp = Linq.Expression.Lambda<Func<T, bool>>(newExpression, propertyExpression.Parameters);
+		return For(exp);
 	}
 }
 
@@ -44,7 +55,21 @@ abstract partial class LinqSpec<T>
 	/// implicitly to an expression by just assigning it or passing it as 
 	/// such to another method.
 	/// </summary>
-	public abstract Expression<Func<T, bool>> Expression { get; }
+	public abstract Linq.Expression<Func<T, bool>> Expression { get; }
+
+	/// <summary>
+	/// The classic specification pattern evaluator.
+	/// This compiles and invokes the expression from the LinqSpec
+	/// so that is can be evaluated in code rather than passed to LinqToX.
+	/// If you find yourself doing this frequently you may want to consider
+	/// using a non-Linq specification which does not store the rule as an
+	/// expression, but as a compiled method instead.
+	/// </summary>
+	/// <param name="entity">The T under test</param>
+	public bool IsSatisfiedBy(T entity)
+	{
+		return Expression.Compile().Invoke(entity);
+	}
 
 	/// <summary>
 	/// Allows to combine two query specifications using a logical And operation.
@@ -83,7 +108,7 @@ abstract partial class LinqSpec<T>
 	/// <summary>
 	/// Performs an implicit conversion from <see cref="LinqSpec{T}"/> to a linq expression.
 	/// </summary>
-	public static implicit operator Expression<Func<T, bool>>(LinqSpec<T> spec)
+	public static implicit operator Linq.Expression<Func<T, bool>>(LinqSpec<T> spec)
 	{
 		return spec.Expression;
 	}
@@ -91,7 +116,7 @@ abstract partial class LinqSpec<T>
 	/// <summary>
 	/// Performs an implicit conversion from a linq expression to <see cref="LinqSpec&lt;T&gt;"/>.
 	/// </summary>
-	public static implicit operator LinqSpec<T>(Expression<Func<T, bool>> expression)
+	public static implicit operator LinqSpec<T>(Linq.Expression<Func<T, bool>> expression)
 	{
 		return new AdHocSpec(expression);
 	}
@@ -101,7 +126,6 @@ abstract partial class LinqSpec<T>
 	/// </summary>
 	private class AndSpec : LinqSpec<T>, IEquatable<AndSpec>
 	{
-		private readonly Expression<Func<T, bool>> expression;
 		private LinqSpec<T> spec1;
 		private LinqSpec<T> spec2;
 
@@ -111,18 +135,18 @@ abstract partial class LinqSpec<T>
 			this.spec2 = spec2;
 
 			// combines the expressions without the need for Expression.Invoke which fails on EntityFramework
-			this.expression = spec1.Expression.And(spec2.Expression);
+			Expression = spec1.Expression.And(spec2.Expression);
 		}
 
-		public override Expression<Func<T, bool>> Expression { get { return this.expression; } }
+		public override Linq.Expression<Func<T, bool>> Expression { get; }
 
 		public override bool Equals(object obj)
 		{
-			if (Object.ReferenceEquals(null, obj)) return false;
-			if (Object.ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
 
-			return Equals((LinqSpec<T>.AndSpec)obj);
+			return Equals((AndSpec) obj);
 		}
 
 		public override int GetHashCode()
@@ -130,10 +154,10 @@ abstract partial class LinqSpec<T>
 			return spec1.GetHashCode() ^ spec2.GetHashCode();
 		}
 
-		public bool Equals(LinqSpec<T>.AndSpec other)
+		public bool Equals(AndSpec other)
 		{
-			return this.spec1.Equals(other.spec1) &&
-				this.spec2.Equals(other.spec2);
+			return spec1.Equals(other.spec1) &&
+						spec2.Equals(other.spec2);
 		}
 	}
 
@@ -142,7 +166,6 @@ abstract partial class LinqSpec<T>
 	/// </summary>
 	private class OrSpec : LinqSpec<T>, IEquatable<OrSpec>
 	{
-		private readonly Expression<Func<T, bool>> expression;
 		private LinqSpec<T> spec1;
 		private LinqSpec<T> spec2;
 
@@ -150,18 +173,18 @@ abstract partial class LinqSpec<T>
 		{
 			this.spec1 = spec1;
 			this.spec2 = spec2;
-			this.expression = spec1.Expression.Or(spec2.Expression);
+			Expression = spec1.Expression.Or(spec2.Expression);
 		}
 
-		public override Expression<Func<T, bool>> Expression { get { return this.expression; } }
+		public override Linq.Expression<Func<T, bool>> Expression { get; }
 
 		public override bool Equals(object obj)
 		{
-			if (Object.ReferenceEquals(null, obj)) return false;
-			if (Object.ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
 
-			return Equals((OrSpec)obj);
+			return Equals((OrSpec) obj);
 		}
 
 		public override int GetHashCode()
@@ -171,10 +194,9 @@ abstract partial class LinqSpec<T>
 
 		public bool Equals(OrSpec other)
 		{
-			return this.spec1.Equals(other.spec1) &&
-				this.spec2.Equals(other.spec2);
+			return spec1.Equals(other.spec1) &&
+						spec2.Equals(other.spec2);
 		}
-
 	}
 
 	/// <summary>
@@ -182,25 +204,24 @@ abstract partial class LinqSpec<T>
 	/// </summary>
 	private class NegateSpec<TArg> : LinqSpec<TArg>, IEquatable<NegateSpec<TArg>>
 	{
-		private readonly Expression<Func<TArg, bool>> expression;
 		private LinqSpec<TArg> spec;
 
 		public NegateSpec(LinqSpec<TArg> spec)
 		{
 			this.spec = spec;
-			this.expression = Linq.Expression.Lambda<Func<TArg, bool>>(
+			Expression = Linq.Expression.Lambda<Func<TArg, bool>>(
 				Linq.Expression.Not(spec.Expression.Body), spec.Expression.Parameters);
 		}
 
-		public override Expression<Func<TArg, bool>> Expression { get { return this.expression; } }
+		public override Linq.Expression<Func<TArg, bool>> Expression { get; }
 
 		public override bool Equals(object obj)
 		{
-			if (Object.ReferenceEquals(null, obj)) return false;
-			if (Object.ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
 
-			return Equals((LinqSpec<T>.NegateSpec<TArg>)obj);
+			return Equals((LinqSpec<T>.NegateSpec<TArg>) obj);
 		}
 
 		public override int GetHashCode()
@@ -210,38 +231,41 @@ abstract partial class LinqSpec<T>
 
 		public bool Equals(LinqSpec<T>.NegateSpec<TArg> other)
 		{
-			return this.spec.Equals(other.spec);
+			return spec.Equals(other.spec);
 		}
 	}
 
 	private class AdHocSpec : LinqSpec<T>, IEquatable<AdHocSpec>
 	{
-		private readonly Expression<Func<T, bool>> specification;
+		private readonly Linq.Expression<Func<T, bool>> specification;
 
-		public AdHocSpec(Expression<Func<T, bool>> specification)
+		public AdHocSpec(Linq.Expression<Func<T, bool>> specification)
 		{
 			this.specification = specification;
 		}
 
-		public override Expression<Func<T, bool>> Expression { get { return this.specification; } }
+		public override Linq.Expression<Func<T, bool>> Expression
+		{
+			get { return specification; }
+		}
 
 		public override bool Equals(object obj)
 		{
-			if (Object.ReferenceEquals(null, obj)) return false;
-			if (Object.ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
 
-			return Equals((AdHocSpec)obj);
+			return Equals((AdHocSpec) obj);
 		}
 
 		public override int GetHashCode()
 		{
-			return this.specification.GetHashCode();
+			return specification.GetHashCode();
 		}
 
 		public bool Equals(AdHocSpec other)
 		{
-			return this.specification.Equals(other.specification);
+			return specification.Equals(other.specification);
 		}
 	}
 }
---
+++ Extensions/Patterns/LinqSpecs/Source/netfx/System/Linq/Expressions/ParameterReplaceVisitor.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+// Code copied from http://stackoverflow.com/questions/26439718/specification-patternmultitype-composite-specifications
+internal class ParameterReplaceVisitor : ExpressionVisitor
+{
+	private readonly ParameterExpression _parameter;
+	private readonly Expression _replacementExpression;
+
+	public ParameterReplaceVisitor(ParameterExpression parameter, Expression replacementExpression)
+	{
+		_parameter = parameter;
+		_replacementExpression = replacementExpression;
+	}
+
+	protected override Expression VisitParameter(ParameterExpression node)
+	{
+		return (node == _parameter) ? _replacementExpression : node;
+	}
+}
