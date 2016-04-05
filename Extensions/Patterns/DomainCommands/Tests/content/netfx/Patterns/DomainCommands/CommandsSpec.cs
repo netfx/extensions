21c00a02ad4d1ea85aeb0e600b905d26868b9400
Daniel Cazzulino <daniel@cazzulino.com>
2016-04-05 15:49:32 -0300

    Fix domain commands tests

--- Extensions/Patterns/DomainCommands/Tests/content/netfx/Patterns/DomainCommands/CommandsSpec.cs
+++ Extensions/Patterns/DomainCommands/Tests/content/netfx/Patterns/DomainCommands/CommandsSpec.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Xunit;
 using Moq;
 
 namespace NetFx.Patterns.DomainCommands
@@ -10,262 +8,262 @@ namespace NetFx.Patterns.DomainCommands
 	public class DomainCommandsSpec
 	{
 		[Fact]
-		public void WhenNoStoreSpecifiedAndMatchingHandlerRegistered_ThenExecuteSucceeds()
+		public void WhenNoStoreSpecifiedAndMatchingHandlerRegistered_ThenExecuteSucceeds ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = Mock.Of<ICommandHandler<FooCommand>>(x => x.IsAsync == true);
 
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler });
 
-			bus.Execute(command);
+			bus.Execute (command);
 		}
 
 		[Fact]
-		public void WhenHandlerRegisteredForSpecificType_ThenHandlesOnExecute()
+		public void WhenHandlerRegisteredForSpecificType_ThenHandlesOnExecute ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = new Mock<CommandHandler<FooCommand>> { CallBase = true };
 
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler.Object });
 
-			bus.Execute(command);
+			bus.Execute (command);
 
-			handler.Verify(x => x.Handle(command, It.IsAny<IDictionary<string, object>>()));
+			handler.Verify (x => x.Handle (command, It.IsAny<IDictionary<string, object>> ()));
 		}
 
 		[Fact]
-		public void WhenHandlerRegisteredForBaseType_ThenThrowsForExecuteWithDerivedClass()
+		public void WhenHandlerRegisteredForBaseType_ThenThrowsForExecuteWithDerivedClass ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = Mock.Of<ICommandHandler<BaseCommand>>();
 
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler });
 
-			Assert.Throws<InvalidOperationException>(() => bus.Execute(command));
+			Assert.Throws<InvalidOperationException> (() => bus.Execute (command));
 		}
 
 		[Fact]
-		public void WhenAsyncHandlerRegisteredForBaseType_ThenThrowsForExecuteWithDerivedClass()
+		public void WhenAsyncHandlerRegisteredForBaseType_ThenThrowsForExecuteWithDerivedClass ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = Mock.Of<ICommandHandler<BaseCommand>>();
 
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler }, action => action());
 
-			Assert.Throws<InvalidOperationException>(() => bus.Execute(command));
+			Assert.Throws<InvalidOperationException> (() => bus.Execute (command));
 		}
 
 		[Fact]
-		public void WhenAsyncHandlerRegisteredForSpecificType_ThenInvokesAsyncRunner()
+		public void WhenAsyncHandlerRegisteredForSpecificType_ThenInvokesAsyncRunner ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = new Mock<CommandHandler<FooCommand>> { CallBase = true };
-			handler.Setup(x => x.IsAsync).Returns(true);
+			handler.Setup (x => x.IsAsync).Returns (true);
 			var asyncCalled = false;
 			Action<Action> asyncRunner = action => asyncCalled = true;
 
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler.Object }, asyncRunner);
 
-			bus.Execute(command);
+			bus.Execute (command);
 
-			handler.Verify(x => x.Handle(command, It.IsAny<IDictionary<string, object>>()), Times.Never());
-			Assert.True(asyncCalled);
+			handler.Verify (x => x.Handle (command, It.IsAny<IDictionary<string, object>> ()), Times.Never ());
+			Assert.True (asyncCalled);
 		}
 
 		[Fact]
-		public void WhenHandlerRegisteredForSpecificType_ThenPersistsToStore()
+		public void WhenHandlerRegisteredForSpecificType_ThenPersistsToStore ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = Mock.Of<ICommandHandler<FooCommand>>();
 			var store = new Mock<IMessageStore<IDomainCommand>>();
 			var bus = new CommandRegistry<IDomainCommand>(store.Object, new[] { handler });
 
-			bus.Execute(command);
+			bus.Execute (command);
 
-			store.Verify(x => x.Save(command, It.IsAny<IDictionary<string, object>>()));
+			store.Verify (x => x.Save (command, It.IsAny<IDictionary<string, object>> ()));
 		}
 
 		[Fact]
-		public void WhenAsyncHandlerRegisteredForSpecificType_ThenCanUseDefaultAsyncRunner()
+		public void WhenAsyncHandlerRegisteredForSpecificType_ThenCanUseDefaultAsyncRunner ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handlerCalled = false;
 			var handler = new Mock<ICommandHandler<FooCommand>>();
-			handler.Setup(x => x.IsAsync).Returns(true);
-			handler.Setup(x => x.Handle(It.IsAny<FooCommand>(), It.IsAny<IDictionary<string, object>>()))
-				.Callback<FooCommand, IDictionary<string, object>>((c, h) => handlerCalled = true);
+			handler.Setup (x => x.IsAsync).Returns (true);
+			handler.Setup (x => x.Handle (It.IsAny<FooCommand> (), It.IsAny<IDictionary<string, object>> ()))
+				.Callback<FooCommand, IDictionary<string, object>> ((c, h) => handlerCalled = true);
 
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler.Object });
 
-			bus.Execute(command);
+			bus.Execute (command);
 
 			while (!handlerCalled)
-				System.Threading.Thread.Sleep(10);
+				System.Threading.Thread.Sleep (10);
 
-			handler.Verify(x => x.Handle(command, It.IsAny<IDictionary<string, object>>()));
+			handler.Verify (x => x.Handle (command, It.IsAny<IDictionary<string, object>> ()));
 		}
 
 		[Fact]
-		public void WhenNoAsyncHandlerRegistereded_ThenInvokesOnThreadPool()
+		public void WhenNoAsyncHandlerRegistereded_ThenInvokesOnThreadPool ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = new Mock<CommandHandler<FooCommand>> { CallBase = true };
-			handler.Setup(x => x.IsAsync).Returns(true);
+			handler.Setup (x => x.IsAsync).Returns (true);
 			var handlerCalled = false;
-			handler.Setup(x => x.Handle(It.IsAny<FooCommand>(), It.IsAny<IDictionary<string, object>>()))
-				.Callback<FooCommand, IDictionary<string, object>>((c, h) => handlerCalled = true);
+			handler.Setup (x => x.Handle (It.IsAny<FooCommand> (), It.IsAny<IDictionary<string, object>> ()))
+				.Callback<FooCommand, IDictionary<string, object>> ((c, h) => handlerCalled = true);
 			var bus = new CommandRegistry<IDomainCommand>(new[] { handler.Object });
 
-			bus.Execute(command);
+			bus.Execute (command);
 
 			while (!handlerCalled)
-				System.Threading.Thread.Sleep(10);
+				System.Threading.Thread.Sleep (10);
 
-			handler.Verify(x => x.Handle(command, It.IsAny<IDictionary<string, object>>()));
+			handler.Verify (x => x.Handle (command, It.IsAny<IDictionary<string, object>> ()));
 		}
 
 		[Fact]
-		public void WhenNoAsyncHandlerRegistereded_ThenInvokesOnThreadPoolAndPersistsToStore()
+		public void WhenNoAsyncHandlerRegistereded_ThenInvokesOnThreadPoolAndPersistsToStore ()
 		{
 			var command = new FooCommand { Id = 5 };
 			var handler = new Mock<ICommandHandler<FooCommand>>();
-			handler.Setup(x => x.IsAsync).Returns(true);
+			handler.Setup (x => x.IsAsync).Returns (true);
 			var handlerCalled = false;
-			handler.Setup(x => x.Handle(It.IsAny<FooCommand>(), It.IsAny<IDictionary<string, object>>()))
-				.Callback<FooCommand, IDictionary<string, object>>((c, h) => handlerCalled = true);
+			handler.Setup (x => x.Handle (It.IsAny<FooCommand> (), It.IsAny<IDictionary<string, object>> ()))
+				.Callback<FooCommand, IDictionary<string, object>> ((c, h) => handlerCalled = true);
 
 			var store = new Mock<IMessageStore<IDomainCommand>>();
 
 			var bus = new CommandRegistry<IDomainCommand>(store.Object, new[] { handler.Object });
 
-			bus.Execute(command);
+			bus.Execute (command);
 
 			while (!handlerCalled)
-				System.Threading.Thread.Sleep(10);
+				System.Threading.Thread.Sleep (10);
 
-			handler.Verify(x => x.Handle(command, It.IsAny<IDictionary<string, object>>()));
-			store.Verify(x => x.Save(command, It.IsAny<IDictionary<string, object>>()));
+			handler.Verify (x => x.Handle (command, It.IsAny<IDictionary<string, object>> ()));
+			store.Verify (x => x.Save (command, It.IsAny<IDictionary<string, object>> ()));
 		}
 
 		[Fact]
-		public void WhenHandlerDoesNotInheritFromGenericHandler_ThenThrows()
+		public void WhenHandlerDoesNotInheritFromGenericHandler_ThenThrows ()
 		{
 			var handler = new NonGenericHandler();
 
-			Assert.Throws<ArgumentException>(() => new CommandRegistry<IDomainCommand>(new ICommandHandler[] { handler }));
+			Assert.Throws<ArgumentException> (() => new CommandRegistry<IDomainCommand> (new ICommandHandler[] { handler }));
 		}
 
 		[Fact]
-		public void WhenDuplicateHandlerIsRegisteredForSameCommandType_ThenThrows()
+		public void WhenDuplicateHandlerIsRegisteredForSameCommandType_ThenThrows ()
 		{
-			Assert.Throws<ArgumentException>(() =>
-				new CommandRegistry<IDomainCommand>(new ICommandHandler[] { new FooCommandHandler(), new FooCommandHandler() }));
+			Assert.Throws<ArgumentException> (() =>
+				 new CommandRegistry<IDomainCommand> (new ICommandHandler[] { new FooCommandHandler (), new FooCommandHandler () }));
 		}
 
 		[Fact]
-		public void WhenNullHandlerProvided_ThenThrowsArgumentException()
+		public void WhenNullHandlerProvided_ThenThrowsArgumentException ()
 		{
-			Assert.Throws<ArgumentException>(() =>
-				new CommandRegistry<IDomainCommand>(new ICommandHandler[] { null }));
+			Assert.Throws<ArgumentException> (() =>
+				 new CommandRegistry<IDomainCommand> (new ICommandHandler[] { null }));
 		}
 
 		[Fact]
-		public void WhenExecuteExtensionOnNullBus_ThenThrowsArgumentNullException()
+		public void WhenExecuteExtensionOnNullBus_ThenThrowsArgumentNullException ()
 		{
-			Assert.Throws<ArgumentNullException>(() =>
-				CommandRegistryExtensions.Execute(
-					default(ICommandRegistry<BaseCommand>),
-					new FooCommand()));
+			Assert.Throws<ArgumentNullException> (() =>
+				 CommandRegistryExtensions.Execute (
+					 default (ICommandRegistry<BaseCommand>),
+					 new FooCommand ()));
 		}
 
 		[Fact]
-		public void WhenExecuteExtensionOnNullBusWithEmptyCommands_ThenThrowsArgumentNullException()
+		public void WhenExecuteExtensionOnNullBusWithEmptyCommands_ThenThrowsArgumentNullException ()
 		{
-			Assert.Throws<ArgumentNullException>(() =>
-				CommandRegistryExtensions.Execute(
-					default(ICommandRegistry<BaseCommand>),
-					Enumerable.Empty<BaseCommand>()));
+			Assert.Throws<ArgumentNullException> (() =>
+				 CommandRegistryExtensions.Execute (
+					 default (ICommandRegistry<BaseCommand>),
+					 Enumerable.Empty<BaseCommand> ()));
 		}
 
 		[Fact]
-		public void WhenExecuteExtensionWithNullCommands_ThenThrowsArgumentNullException()
+		public void WhenExecuteExtensionWithNullCommands_ThenThrowsArgumentNullException ()
 		{
-			Assert.Throws<ArgumentNullException>(() =>
-				CommandRegistryExtensions.Execute(
-					Mock.Of<ICommandRegistry<BaseCommand>>(),
-					default(IEnumerable<BaseCommand>)));
+			Assert.Throws<ArgumentNullException> (() =>
+				 CommandRegistryExtensions.Execute (
+					 Mock.Of<ICommandRegistry<BaseCommand>> (),
+					 default (IEnumerable<BaseCommand>)));
 		}
 
 		[Fact]
-		public void WhenExecuteExtensionWithNullHeadersAndCommands_ThenThrowsArgumentNullException()
+		public void WhenExecuteExtensionWithNullHeadersAndCommands_ThenThrowsArgumentNullException ()
 		{
-			Assert.Throws<ArgumentNullException>(() =>
-				CommandRegistryExtensions.Execute(
-					Mock.Of<ICommandRegistry<BaseCommand>>(),
-					Enumerable.Empty<BaseCommand>(),
-					default(IDictionary<string, object>)));
+			Assert.Throws<ArgumentNullException> (() =>
+				 CommandRegistryExtensions.Execute (
+					 Mock.Of<ICommandRegistry<BaseCommand>> (),
+					 Enumerable.Empty<BaseCommand> (),
+					 default (IDictionary<string, object>)));
 		}
 
 		[Fact]
-		public void WhenExecuteExtensionWithHeadersAndCommands_ThenCopiesDictionary()
+		public void WhenExecuteExtensionWithHeadersAndCommands_ThenCopiesDictionary ()
 		{
 			var headers = new Dictionary<string, object>();
-			headers.Add("IP", "localhost");
+			headers.Add ("IP", "localhost");
 
 			var bus = new Mock<ICommandRegistry<BaseCommand>>();
 
-			CommandRegistryExtensions.Execute(
+			CommandRegistryExtensions.Execute (
 					bus.Object,
-					new BaseCommand[] { new FooCommand() },
+					new BaseCommand[] { new FooCommand () },
 					headers);
 
-			bus.Verify(x => x.Execute(It.IsAny<BaseCommand>(), It.Is<IDictionary<string, object>>(dict => Object.ReferenceEquals(dict, headers))), Times.Never());
-			bus.Verify(x => x.Execute(It.IsAny<BaseCommand>(), It.Is<IDictionary<string, object>>(dict =>
-				dict.ContainsKey("IP") && ((string)dict["IP"]) == "localhost")));
+			bus.Verify (x => x.Execute (It.IsAny<BaseCommand> (), It.Is<IDictionary<string, object>> (dict => Object.ReferenceEquals (dict, headers))), Times.Never ());
+			bus.Verify (x => x.Execute (It.IsAny<BaseCommand> (), It.Is<IDictionary<string, object>> (dict =>
+					dict.ContainsKey ("IP") && ((string)dict["IP"]) == "localhost")));
 		}
 
 		[Fact]
-		public void WhenExecuteExtensionWithCommands_ThenCreatesNewHeadersForEach()
+		public void WhenExecuteExtensionWithCommands_ThenCreatesNewHeadersForEach ()
 		{
 			var headers = new HashSet<IDictionary<string, object>>();
 			var bus = new Mock<ICommandRegistry<BaseCommand>>();
 
-			bus.Setup(x => x.Execute(It.IsAny<BaseCommand>(), It.IsAny<IDictionary<string, object>>()))
-				.Callback((BaseCommand cmd, IDictionary<string, object> h) => headers.Add(h));
+			bus.Setup (x => x.Execute (It.IsAny<BaseCommand> (), It.IsAny<IDictionary<string, object>> ()))
+				.Callback ((BaseCommand cmd, IDictionary<string, object> h) => headers.Add (h));
 
-			CommandRegistryExtensions.Execute(
+			CommandRegistryExtensions.Execute (
 					bus.Object,
-					new BaseCommand[] { new FooCommand(), new FooCommand() });
+					new BaseCommand[] { new FooCommand (), new FooCommand () });
 
-			Assert.Equal(2, headers.Count);
+			Assert.Equal (2, headers.Count);
 			var dupe = new Dictionary<string, object>();
-			headers.Add(dupe);
-			headers.Add(dupe);
+			headers.Add (dupe);
+			headers.Add (dupe);
 
-			Assert.Equal(3, headers.Count);
+			Assert.Equal (3, headers.Count);
 		}
 
 		[Fact]
-		public void WhenExecutedCommandThrows_ThenSavesExceptionAsHeader()
+		public void WhenExecutedCommandThrows_ThenSavesExceptionAsHeader ()
 		{
 			var headers = new Dictionary<string, object>();
-			headers.Add("IP", "localhost");
+			headers.Add ("IP", "localhost");
 
 			var handler = new Mock<ICommandHandler<FooCommand>>();
 			var store = new Mock<IMessageStore<IDomainCommand>>();
 			var registry = new CommandRegistry<IDomainCommand>(store.Object, new[] { handler.Object });
 
-			handler.Setup(x => x.Handle(It.IsAny<FooCommand>(), headers))
-				.Throws(new ArgumentException("foo"));
+			handler.Setup (x => x.Handle (It.IsAny<FooCommand> (), headers))
+				.Throws (new ArgumentException ("foo"));
 
-			registry.Execute(new FooCommand(), headers);
+			registry.Execute (new FooCommand (), headers);
 
-			Assert.True(headers.ContainsKey("Exception"));
+			Assert.True (headers.ContainsKey ("Exception"));
 		}
 
 		private class FooCommandHandler : CommandHandler<FooCommand>
 		{
-			public override void Handle(FooCommand command, IDictionary<string, object> headers)
+			public override void Handle (FooCommand command, IDictionary<string, object> headers)
 			{
 			}
 
