68cae03ebb08ada14b5eb485375e20f9bc7761da
Daniel Cazzulino <daniel@cazzulino.com>
2019-12-30 15:50:57 -0300

    Port to NS2 to support .NETCore builds

---
+++ Extensions/System/StringResources/Source/T4Fixes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Threading;
+
+internal static class T4Extensions
+{
+	public static MethodInfo GetMethod(this Type type, string method, params Type[] parameters)
+		=> type.GetRuntimeMethod(method, parameters);
+}
+
+namespace System.CodeDom.Compiler
+{
+	/// <summary>Internal</summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public class CompilerErrorCollection : List<CompilerError>
+	{
+		/// <summary>Internal</summary>
+		public bool HasErrors => Count > 0;
+	}
+
+	/// <summary>Internal</summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public class CompilerError
+	{
+		/// <summary>Internal</summary>
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public string ErrorText { get; set; }
+
+		/// <summary>Internal</summary>
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public bool IsWarning { get; set; }
+	}
+}
+
+namespace System.Runtime.Remoting.Messaging
+{
+	/// <summary>
+	/// Provides a way to set contextual data that flows with the call and 
+	/// async context of a test or invocation.
+	/// </summary>
+	public static class CallContext
+	{
+		static ConcurrentDictionary<string, AsyncLocal<object>> state = new ConcurrentDictionary<string, AsyncLocal<object>>();
+
+		/// <summary>
+		/// Stores a given object and associates it with the specified name.
+		/// </summary>
+		/// <param name="name">The name with which to associate the new item in the call context.</param>
+		/// <param name="data">The object to store in the call context.</param>
+		public static void LogicalSetData(string name, object data) =>
+			state.GetOrAdd(name, _ => new AsyncLocal<object>()).Value = data;
+
+		/// <summary>
+		/// Retrieves an object with the specified name from the <see cref="CallContext"/>.
+		/// </summary>
+		/// <param name="name">The name of the item in the call context.</param>
+		/// <returns>The object in the call context associated with the specified name, or <see langword="null"/> if not found.</returns>
+		public static object LogicalGetData(string name) =>
+			state.TryGetValue(name, out AsyncLocal<object> data) ? data.Value : null;
+	}
+}
